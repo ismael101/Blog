356f2e9d8bf8c38fa59d8b878236409e07a5b3e2
ismael101 <ismaelomermohamed@gmail.com>
2019-11-27 22:48:54 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+const articleRoutes = require('./routes/articles')
+const morgan = require('morgan')
+const app = express()
+const path = require('path')
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+app.use(express.static(path.join(__dirname, 'client', 'dist')))
+
+app.use('/images', express.static('images/'))
+app.use('/articles',articleRoutes)
+
+app.use((req,res,next) => {
+    res.status(400).json('Not Found')
+})
+
+app.use((err,req,res,next) => {
+    console.log(err.stack)
+    res.status(500).json(err)
+})
+
+let server = app.listen(process.env.PORT || 5000, () => {
+    console.log(`server running on port ${server.address().port}`)
+})
