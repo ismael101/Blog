1e96f809668685fc3cf71021f1a3f5c509a53543
ismael101 <ismaelomermohamed@gmail.com>
2020-04-20 17:21:47 -0500

    added express server

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const path = require('path')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+//static build folder
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+// Error handlers - for not found, and app errors 
+app.use(function(req, res, next){
+    res.status(404).send('Not found')
+})
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Server error')
+})
+
+// Start server running 
+var server = app.listen(process.env.PORT || 3000, function() {
+    console.log('app running on port', server.address().port)
+})
